db10cbd9d5fb6245556b95bf28aefcd1483448d1
Nerevarishe <JIyHaTuK@gmail.com>
2020-06-27 15:24:03 +0300

    change css to module system

--- src/containers/Main/Dashboard/TasksWidget/TaskUser/TaskUser.js
+++ src/containers/Main/Dashboard/TasksWidget/TaskUser/TaskUser.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./TaskUser.scss";
+import styles from "./TaskUser.module.scss";
 import ViewUser from "../../../../../components/ViewUser";
 
 const TaskUser = (props) => <ViewUser avatar={props.avatar} name={props.name} />;
