00689322deeec89a85e9c2f01d65daac9c117e3c
Nerevarishe <JIyHaTuK@gmail.com>
2020-06-27 11:44:56 +0300

    change css to module system

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -3,65 +3,75 @@ import Ellipse from "./img/svg/Ellipse.svg";
 import Ellipse1 from "./img/svg/Ellipse-1.svg";
 import EditImg from "./img/svg/edit.svg";
 import DeleteImg from "./img/svg/delete.svg";
-import "./TaskCard.scss";
+import styles from "./TaskCard.module.scss";
 import TaskUser from "../../containers/Main/Dashboard/TasksWidget/TaskUser";
-
 
 const TaskCard = (props) => {
   const checkStatus = (taskStatus) => {
     if (taskStatus === "Completed") {
-      return "tasks-widget__task-card-status tasks-widget__task-card-status_completed";
+      return `${styles["tasks-widget__task-card-status"]} ${styles["tasks-widget__task-card-status_completed"]}`;
     } else if (taskStatus === "Active") {
-      return "tasks-widget__task-card-status tasks-widget__task-card-status_active";
+      return `${styles["tasks-widget__task-card-status"]} ${styles["tasks-widget__task-card-status_active"]}`;
     }
-    return "tasks-widget__task-card-status tasks-widget__task-card-status_ended";
+    return `${styles["tasks-widget__task-card-status"]} ${styles["tasks-widget__task-card-status_ended"]}`;
   };
 
   return (
-    <div className="tasks-widget__task-card">
-      <span className="tasks-widget__task-card-type">
+    <div className={styles["tasks-widget__task-card"]}>
+      <span className={styles["tasks-widget__task-card-type"]}>
         {props.data.taskType}
       </span>
 
-      <span className="tasks-widget__task-card-task">{props.data.task}</span>
+      <span className={styles["tasks-widget__task-card-task"]}>
+        {props.data.task}
+      </span>
 
-      <div className="tasks-widget__task-card-due-date">
-        <span className="tasks-widget__task-card-due-date-text">
+      <div className={styles["tasks-widget__task-card-due-date"]}>
+        <span className={styles["tasks-widget__task-card-due-date-text"]}>
           Due date: {props.data.dueDate}
         </span>
 
-        <span className="tasks-widget__task-card-due-date-date">
+        <span className={styles["tasks-widget__task-card-due-date-date"]}>
           December 23, 2018
         </span>
       </div>
-      <TaskUser avatar={props.data.person.avatar} name={props.data.person.name} />
-      <div className="tasks-widget__task-card-edit-block">
-        <div className="tasks-widget__task-card-edit-block-circles">
+      <TaskUser
+        avatar={props.data.person.avatar}
+        name={props.data.person.name}
+      />
+      <div className={styles["tasks-widget__task-card-edit-block"]}>
+        <div className={styles["tasks-widget__task-card-edit-block-circles"]}>
           <img
             src={Ellipse}
             alt="circle1"
-            className="tasks-widget__task-card-edit-block-circle-yellow"
+            className={
+              styles["tasks-widget__task-card-edit-block-circle-yellow"]
+            }
           />
           <img
             src={Ellipse1}
             alt="circle2"
-            className="tasks-widget__task-card-edit-block-circle-green"
+            className={
+              styles["tasks-widget__task-card-edit-block-circle-green"]
+            }
           />
         </div>
 
-        <div className="tasks-widget__task-card-edit-block-edit-delete">
+        <div
+          className={styles["tasks-widget__task-card-edit-block-edit-delete"]}
+        >
           <a href="#">
             <img
               src={EditImg}
               alt="Edit Task"
-              className="tasks-widget__task-card-edit-block-edit"
+              className={styles["tasks-widget__task-card-edit-block-edit"]}
             />
           </a>
           <a href="#">
             <img
               src={DeleteImg}
               alt="Delete Task"
-              className="tasks-widget__task-card-edit-block-delete"
+              className={styles["tasks-widget__task-card-edit-block-delete"]}
             />
           </a>
         </div>
