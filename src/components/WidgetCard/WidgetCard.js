aa4581fbc51a98bed9aa0e62b41d04e9aae15c9d
Link <Nerevarishe@users.noreply.github.com>
2020-06-28 12:43:48 +0300

    adding props for styling

--- src/components/WidgetCard/WidgetCard.js
+++ src/components/WidgetCard/WidgetCard.js
@@ -1,61 +1,50 @@
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import stylesOriginal from "./WidgetCard.module.scss";
+import styles from "./WidgetCard.module.scss";
 
 // const widgetContent = {
 //   title: "Title",
 //   filterOptions: ["option one", "option two", "option three", "option four"],
 // };
 
-let styles = stylesOriginal;
-
 const WidgetCard = (props) => {
-  const checkStyles = (props) => {
-    if (props) {
-      styles = props;
-      console.dir(styles);
-    }
-  };
-
   return (
-    <Fragment>
-      {checkStyles(props.styles)}
-      <div className={styles["widget-card"]}>
-        {props.showTitle ? (
-          props.customTitle ? (
-            props.customTitle
-          ) : (
-            <span className={styles["widget-card__title-text"]}>
-              {props.widgetContent.title}
-            </span>
-          )
-        ) : null}
+   <div style={styleWidgetCard} className={styles["widget-card"]}>
+      {props.showTitle ? (
+        props.customTitle ? (
+          props.customTitle
+        ) : (
+          <span className={styles["widget-card__title-text"]}>
+            {props.widgetContent.title}
+          </span>
+        )
+      ) : null}
 
-        {props.showFilter ? (
-          <div className={styles["widget-card__filter"]}>
-            <span className={styles["widget-card__filter-text"]}>Show: </span>
+      {props.showFilter ? (
+        <div className={styles["widget-card__filter"]}>
+          <span className={styles["widget-card__filter-text"]}>Show: </span>
 
-            <select
-              name=""
-              id=""
-              className={styles["widget-card__filter-select"]}
-            >
-              {props.widgetContent.filterOptions.map((option) => (
-                <option
-                  value={option.value}
-                  className={styles["widget-card__filter-option"]}
-                >
-                  {option.text}
-                </option>
-              ))}
-            </select>
-          </div>
-        ) : null}
+          <select
+            style={props.styleWidgetCardFilterSelect}
+            name=""
+            id=""
+            className={styles["widget-card__filter-select"]}
+          >
+            {props.widgetContent.filterOptions.map((option) => (
+              <option
+                value={option.value}
+                className={styles["widget-card__filter-option"]}
+              >
+                {option.text}
+              </option>
+            ))}
+          </select>
+        </div>
+      ) : null}
 
-        <div className={styles["widget-card__divider"]} />
-        <div className={styles["widget-card__content"]}>{props.children}</div>
-      </div>
-    </Fragment>
+      <div className={styles["widget-card__divider"]} />
+      <div className={styles["widget-card__content"]}>{props.children}</div>
+    </div>
   );
 };
 
@@ -64,6 +53,13 @@ WidgetCard.propTypes = {
   customTitle: PropTypes.element,
   showFilter: PropTypes.bool,
   widgetContent: PropTypes.object,
+  styleWidgetCard: PropTypes.object,
+  styleWidgetCardFilterSelect: PropsTypes.object
 };
+
+WidgetCard.defaultProps = {
+  styleWidgetCard: null,
+  styleWidgetCardFilterSelect: null
+}
 
 export default WidgetCard;
