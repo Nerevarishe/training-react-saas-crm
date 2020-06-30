ebf44ad0e6974fe8a948dedd87b15e77de82d87a
Nerevarishe <JIyHaTuK@gmail.com>
2020-06-30 12:25:57 +0300

    styled-components in PropgressBar

--- src/containers/Main/Dashboard/TasksWidget/ProgressBar/index.js
+++ src/containers/Main/Dashboard/TasksWidget/ProgressBar/index.js
@@ -1,1 +1,14 @@
-export { default } from "./ProgressBar";
+import React from "react";
+import PropTypes from "prop-types";
+import { ProgressBarStyled } from "./ProgressBarStyled";
+
+const ProgressBar = (props) => {
+  return <ProgressBarStyled max={props.max} value={props.value} />;
+};
+
+ProgressBar.propTypes = {
+  max: PropTypes.number,
+  value: PropTypes.number,
+};
+
+export default ProgressBar;
