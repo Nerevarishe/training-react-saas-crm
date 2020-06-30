e4597b3231749da005a722df02133316e43e832c
Nerevarishe <JIyHaTuK@gmail.com>
2020-06-30 23:29:35 +0300

    styled-components for Header

---
+++ src/containers/Header/HeaderStyled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  position: fixed;
+  background: ${(props) => props.theme.colors.whiteColor}};
+  z-index: 100;
+`;
--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,1 +1,15 @@
-export { default } from "./Header";
+import React from "react";
+import GlobalSearch from "./GlobalSearch";
+import SideBar from "./SideBar";
+import { HeaderStyled } from "./HeaderStyled";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <GlobalSearch />
+      <SideBar />
+    </HeaderStyled>
+  );
+};
+
+export default Header;
