151fcab92ab867a54747b688c169c21ca1df22e6
Nerevarishe <JIyHaTuK@gmail.com>
2020-06-30 23:32:46 +0300

    styled-components for Main

---
+++ src/containers/Main/MainStyled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const MainStyled = styled.main`
+  display: flex;
+  padding: 96px 38px 36px 292px;
+`;
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,1 +1,13 @@
-export { default } from "./Main";
+import React from "react";
+import Dashboard from "./Dashboard";
+import { MainStyled } from "./MainStyled";
+
+const Main = () => {
+  return (
+    <MainStyled>
+      <Dashboard />
+    </MainStyled>
+  );
+};
+
+export default Main;
