51de4b2edd5862290d821a44605fa00e259aa1c4
Nerevarishe <JIyHaTuK@gmail.com>
2020-07-04 12:05:51 +0300

    centring icons in pulled in SideBar

--- src/containers/Header/SideBar/SideBarItem/SideBarLinkIcon.js
+++ src/containers/Header/SideBar/SideBarItem/SideBarLinkIcon.js
@@ -2,9 +2,12 @@ import styled from "styled-components";
 import withContext from "../../../../hoc/withContext";
 
 const SideBarLinkIcon = styled.img`
+  display: ${(props) => (props.state.isSideBarPulledOut ? "initial" : "block")};
   width: 20px;
   height: 20px;
   margin-right: ${(props) => (props.state.isSideBarPulledOut ? "14px" : "0")}};
+  margin: ${(props) =>
+    props.state.isSideBarPulledOut ? "0 14px 0 0" : "0 auto"}};
   vertical-align: middle;
 `;
 
