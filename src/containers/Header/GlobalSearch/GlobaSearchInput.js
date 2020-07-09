5eb82bd1d05b980888557d42d4e49795dbf055eb
Nerevarishe <JIyHaTuK@gmail.com>
2020-07-09 23:09:09 +0300

    fix GlobalSearchBar styles

--- src/containers/Header/GlobalSearch/GlobaSearchInput.js
+++ src/containers/Header/GlobalSearch/GlobaSearchInput.js
@@ -1,18 +1,18 @@
 import styled from "styled-components";
+import { Regular12Font } from "../../../components/Fonts/Fonts";
 
-export const GlobalSearchInput = styled.input`
+export const GlobalSearchInput = styled(Regular12Font)`
   width: 500px;
   height: 20px;
   border: none;
   padding-left: 32px;
-  background: url("./img/svg/search.svg") no-repeat;
-  @include regular-12-font;
+  background: url(${(props) => props.icon}) no-repeat;
 
   &:focus {
     outline-style: none;
   }
 
   &::placeholder {
-    color: $gray-color;
+    color: ${(props) => props.theme.colors.grayColor};
   }
 `;
--- src/containers/Header/GlobalSearch/GlobalSearch.js
+++ src/containers/Header/GlobalSearch/GlobalSearch.js
@@ -1,4 +1,5 @@
 import React from "react";
+import searchIcon from "./img/svg/search.svg";
 import notificationBell from "./img/svg/notification-bell.svg";
 import notificationDot from "./img/svg/notification-red-dot.svg";
 import GlobalSearchStyled from "./GlobalSearchStyled";
@@ -9,7 +10,12 @@ import { GlobalSearchNotificationDot } from "./GlobalSearchNotificationDot";
 const GlobalSearch = () => {
   return (
     <GlobalSearchStyled>
-      <GlobalSearchInput type="text" placeholder="Global search" />
+      <GlobalSearchInput
+        as="input"
+        type="text"
+        placeholder="Global search"
+        icon={searchIcon}
+      />
       <GlobalSearchNotification>
         <img src={notificationBell} alt="Notification" />
         <GlobalSearchNotificationDot src={notificationDot} alt="" show />
